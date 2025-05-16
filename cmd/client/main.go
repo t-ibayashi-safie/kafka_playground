a8928df2e58e848b3fa883b21f3950b2cd4bd3da
t-ibayashi-safie <t-ibayashi@safie.jp>
2025-05-16 10:33:58 +0900

    ManualCommitConsumer 追加

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -11,7 +11,8 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := consumer.RunSimpleConsumer(ctx)
+	// err := consumer.RunSimpleConsumer(ctx)
+	err := consumer.ManualCommitConsumer(ctx)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
